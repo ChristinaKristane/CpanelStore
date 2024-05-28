1d24ed6584dcf4eb71cb9d0d953bd17b2dc7e011
ChristinaKristane <65531624+ChristinaKristane@users.noreply.github.com>
2024-05-28 17:06:05 +0000

    fix tests

--- tests/e2e/add-addon-to-the-cart.spec.ts
+++ tests/e2e/add-addon-to-the-cart.spec.ts
@@ -9,6 +9,8 @@ test.describe('Addon product', () => {
 
   const ip = '2.2.2.2'
   const cpanel = 'cPanel Solo® Cloud (1 Account)'
+  const total_amount = '$2.26 USD'
+  const product_price = '$17.49 USD'
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page)
@@ -23,37 +25,25 @@ test.describe('Addon product', () => {
     const configure_stage = new ConfigureStage(page)
     const review_stage = new ReviewStage(page)
     const checkout_stage = new CheckoutStage(page)
-    await test.step(
-      'Given I navigate as "user" to cPanel and I click on "Browse products" on cPanel Licenses',
-      async () => {
-        await homePage.cpanel_browse_products()
-      }
-    )
-
-    await test.step('And I verify that Total amount ', async () => {
-      await expect(configure_stage.product).toContainText('$17.49 USD')
+    await test.step('Given I navigate as "user" to cPanel and I click on "Browse products" on cPanel Licenses', async () => {
+      await homePage.cpanel_browse_products()
     })
 
-    await test.step(
-      'When I click "Order Now" on cPanel Solo® Cloud',
-      async () => {
-        await configure_stage.order_now(0)
-      }
-    )
+    await test.step('And I verify that Total amount ', async () => {
+      await expect(configure_stage.product).toContainText(product_price)
+    })
 
-    await test.step(
-      'And I enter "2.2.2.2" as an IP address in the Additional Information',
-      async () => {
-        await configure_stage.fill_ip(ip)
-      }
-    )
+    await test.step('When I click "Order Now" on cPanel Solo® Cloud', async () => {
+      await configure_stage.order_now(0)
+    })
 
-    await test.step(
-      'And I add "Available Addons" by clicking on "Add to Cart" button',
-      async () => {
-        await configure_stage.available_addod(1)
-      }
-    )
+    await test.step('And I enter "2.2.2.2" as an IP address in the Additional Information', async () => {
+      await configure_stage.fill_ip(ip)
+    })
+
+    await test.step('And I add "Available Addons" by clicking on "Add to Cart" button', async () => {
+      await configure_stage.available_addod(1)
+    })
     await test.step('And I click to "Continue" button', async () => {
       await configure_stage.continue_button()
     })
@@ -61,106 +51,68 @@ test.describe('Addon product', () => {
     await test.step('And I click on "Checkout" button', async () => {
       await review_stage.checkout.click()
     })
-    await test.step(
-      'Then I verify that Produc/Options is contain correct monthly price for cPanel Solo® Cloud (1 Account)',
-      async () => {
-        await expect(checkout_stage.table).toContainText('$17.49 USD')
-        await expect(checkout_stage.table).toContainText(cpanel)
-      }
-    )
+    await test.step('Then I verify that Produc/Options is contain correct monthly price for cPanel Solo® Cloud (1 Account)', async () => {
+      await expect(checkout_stage.table).toContainText(product_price)
+      await expect(checkout_stage.table).toContainText(cpanel)
+    })
 
-    await test.step(
-      'And I verify that subtotal amount is $2.82 USD Total Due Today',
-      async () => {
-        await expect(checkout_stage.subtotal).toContainText('$2.82')
-      }
-    )
+    await test.step('And I verify that subtotal amount is $2.82 USD Total Due Today', async () => {
+      await expect(checkout_stage.subtotal).toContainText(total_amount)
+    })
   })
 
-  test.only('Product without addon has added to the cart successfully and has correct subtotal price @regression', async ({
+  test.only('Product without addon has added to the cart successfully and has correct price on Review and Checkout stage @regression', async ({
     page,
   }) => {
     const configure_stage = new ConfigureStage(page)
     const review_stage = new ReviewStage(page)
     const checkout_stage = new CheckoutStage(page)
-    const total_amount = '$2.82 USD'
 
-    await test.step(
-      'Given I navigate as "user" to cPanel and I click on "Browse products" on cPanel Licenses',
-      async () => {
-        await homePage.cpanel_browse_products()
-      }
-    )
+    await test.step('Given I navigate as "user" to cPanel and I click on "Browse products" on cPanel Licenses', async () => {
+      await homePage.cpanel_browse_products()
+    })
 
-    await test.step(
-      'When I click "Order Now" on cPanel Solo® Cloud',
-      async () => {
-        await configure_stage.order_now(0)
-      }
-    )
+    await test.step('When I click "Order Now" on cPanel Solo® Cloud', async () => {
+      await configure_stage.order_now(0)
+    })
 
-    await test.step(
-      'And I enter "2.2.2.2" as an IP address in the Additional Information',
-      async () => {
-        await configure_stage.fill_ip(ip)
-      }
-    )
+    await test.step('And I enter "2.2.2.2" as an IP address in the Additional Information', async () => {
+      await configure_stage.fill_ip(ip)
+    })
 
     await test.step('And I click to "Continue" button', async () => {
       await configure_stage.continue_button()
     })
 
-    await test.step(
-      'Then I verify that products are present on summary in review stage and contain $2.82 USD  USD Total Due Today before I go to the checkout stage',
-      async () => {
-        await review_stage.review_summary(total_amount)
+    await test.step('Then I verify that product cPanel Solo® Cloud (1 Account) is present on summary in review stage and contain $17.49 USD before I go to the checkout stage', async () => {
+      await expect(review_stage.order_summary).toContainText(product_price)
 
-        await expect(review_stage.order_summary).toContainText(total_amount)
-        await expect(review_stage.order_summary).toContainText(cpanel)
-      }
-    )
+      await expect(review_stage.order_summary).toContainText(cpanel)
+    })
 
     await test.step('And I click on "Checkout" button', async () => {
       await review_stage.checkout.click()
     })
-    await test.step(
-      'Then I verify that table on Checkout stage is contain IP adress "2.2.2.2"',
-      async () => {
-        await expect(checkout_stage.table).toContainText(ip)
-      }
-    )
+    await test.step('Then I verify that table on Checkout stage is contain IP adress "2.2.2.2"', async () => {
+      await expect(checkout_stage.table).toContainText(ip)
+    })
 
-    await test.step(
-      'And I verify that table on Checkout stage is contain added product',
-      async () => {
-        await expect(checkout_stage.table).toContainText(cpanel)
-      }
-    )
+    await test.step('And I verify that table on Checkout stage is contain added product', async () => {
+      await expect(checkout_stage.table).toContainText(cpanel)
+    })
 
-    await test.step(
-      'Then I verify that subtotal amount is $2.82 USD Total Due Today',
-      async () => {
-        await expect(checkout_stage.subtotal).toContainText(total_amount)
-      }
-    )
-    await test.step(
-      'When I enter first name into Personal information',
-      async () => {
-        await checkout_stage.fill_first_name('Galtsova Krystyna')
-      }
-    )
-    await test.step(
-      'Then I verify that Personal information and Billing Address sections are visible',
-      async () => {
-        await expect(checkout_stage.first_name).toHaveValue('Galtsova Krystyna')
-        await expect(checkout_stage.company_name.isVisible()).toBeTruthy()
-      }
-    )
-    await test.step(
-      'And I verify that "Complete order" button is disabled',
-      async () => {
-        await expect(checkout_stage.complete.isDisabled()).toBeTruthy()
-      }
-    )
+    await test.step('Then I verify that amount of cPanel Solo® Cloud (1 Account) product is $17.49 USD', async () => {
+      await expect(checkout_stage.table).toContainText(product_price)
+    })
+    await test.step('When I enter first name into Personal information', async () => {
+      await checkout_stage.fill_first_name('Galtsova Krystyna')
+    })
+    await test.step('Then I verify that Personal information and Billing Address sections are visible', async () => {
+      await expect(checkout_stage.first_name).toHaveValue('Galtsova Krystyna')
+      await expect(checkout_stage.company_name.isVisible()).toBeTruthy()
+    })
+    await test.step('And I verify that "Complete order" button is disabled', async () => {
+      await expect(checkout_stage.complete.isDisabled()).toBeTruthy()
+    })
   })
 })
